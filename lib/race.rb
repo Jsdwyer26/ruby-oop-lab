8cff4d983d1a049f44995027474856ed688fc4da
Jsdwyer26 <Jsdwyer26@gmail.com>
2015-12-09 21:20:00 -0800

    Weds. lab. All test pass

--- lib/race.rb
+++ lib/race.rb
@@ -2,6 +2,32 @@ require_relative "car"
 
 class Race
 
-  # write Race class code here
+  def initialize
+  	@cars = [Car.new, Car.new]
+
+  	num = Random.new
+  	@cars[0].accelerate(num.rand(10))
+  	@cars[1].accelerate(num.rand(10))
+  end
+
+	def cars
+  		@cars
+	end
+
+	def winner
+	  if cars[0].speed < cars[1].speed[0]
+	    cars[1]
+	  else
+	  	cars[0]
+	  end
+	end
+
+	def loser
+		if winner == cars[1]
+			cars[0]
+		else 
+			cars[1]
+		end
+	end
 
 end
